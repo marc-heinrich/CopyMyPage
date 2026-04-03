6004adad8841f3449129de691bc992f9d25433ca
marc-heinrich <marcy-marc10@gmx.de>
2026-04-03 16:33:49 +0200

    see #69

--- src/com_copymypage/site/tmpl/gallery/default.php
+++ src/com_copymypage/site/tmpl/gallery/default.php
@@ -18,33 +18,30 @@ use Joomla\CMS\Layout\LayoutHelper;
 <section id="gallery-view" class="cmp-gallery-view">
     <div class="uk-container">
         <div class="cmp-gallery-view__header">
-            <a class="cmp-gallery-view__back" href="<?php echo htmlspecialchars($this->backUrl, ENT_QUOTES, 'UTF-8'); ?>">
-                <span uk-icon="icon: arrow-left" aria-hidden="true"></span>
-                <span><?php echo htmlspecialchars(Text::_('COM_COPYMYPAGE_VIEW_GALLERY_BACK'), ENT_QUOTES, 'UTF-8'); ?></span>
-            </a>
-
             <h1 class="cmp-gallery-view__title">
                 <?php echo htmlspecialchars($this->headline, ENT_QUOTES, 'UTF-8'); ?>
             </h1>
 
+            <hr class="cmp-gallery-view__divider uk-divider-small">
+
             <?php if ($this->summary !== '') : ?>
-                <p class="cmp-gallery-view__lead">
-                    <?php echo htmlspecialchars($this->summary, ENT_QUOTES, 'UTF-8'); ?>
+                <p class="cmp-gallery-view__meta">
+                    <span class="cmp-gallery-view__meta-icon" uk-icon="icon: calendar" aria-hidden="true"></span>
+                    <span class="cmp-gallery-view__meta-text">
+                        <?php echo htmlspecialchars($this->summary, ENT_QUOTES, 'UTF-8'); ?>
+                    </span>
                 </p>
             <?php endif; ?>
 
-            <?php if ($this->imageCount > 0) : ?>
-                <p class="cmp-gallery-view__meta">
-                    <?php echo $this->imageCount; ?>
-                    <?php echo htmlspecialchars(Text::_('COM_COPYMYPAGE_VIEW_GALLERY_IMAGES'), ENT_QUOTES, 'UTF-8'); ?>
-                </p>
-            <?php endif; ?>
+            <a class="cmp-gallery-view__back" href="<?php echo htmlspecialchars($this->backUrl, ENT_QUOTES, 'UTF-8'); ?>">
+                <span><?php echo htmlspecialchars(Text::_('COM_COPYMYPAGE_VIEW_GALLERY_BACKBUTTON'), ENT_QUOTES, 'UTF-8'); ?></span>
+            </a>
         </div>
 
         <?php if (!empty($this->warnings)) : ?>
             <?php echo LayoutHelper::render('copymypage.system.warning', ['messages' => $this->warnings]); ?>
         <?php else : ?>
-            <div class="cmp-gallery-view__content">
+            <div class="cmp-gallery-view__content cmp-gallery-view__content--instagram-grid">
                 <?php
                 echo HTMLHelper::_(
                     'content.prepare',
